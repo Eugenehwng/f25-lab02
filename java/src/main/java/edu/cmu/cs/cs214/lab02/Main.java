5f003449314bcf4b67a1fe268d667081ecbdbe01
eugenehwng <eugenehw@andrew.cmu.edu>
2025-12-01 00:35:45 -0500

    final encapsulation update for java files

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,14 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(3);
+        Shape circle = new Circle(3);
 
-        Renderer renderer = new Renderer(rectangle);
-
+        Renderer renderer = new Renderer();
+        renderer.draw();
+        renderer.setShape(square);
+        renderer.draw();
+        renderer.setShape(circle);
         renderer.draw();
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -5,11 +5,24 @@ import edu.cmu.cs.cs214.lab02.shapes.Shape;
 public class Renderer {
     private Shape shape;
     
+    Renderer() {
+        this.shape = null;
+    }
+
     Renderer(Shape shape) {
         this.shape = shape;
     }
 
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
     void draw() {
+        if (this.shape == null) {
+            System.out.println("No shape set");
+            return;
+        }
+        
         double area = shape.getArea();
 
         // assume implementation
